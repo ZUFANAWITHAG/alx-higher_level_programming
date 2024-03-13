26cf2ccfad7115eef004c9e2432fb78c5865b275
Zufan <zufanawushu@gmail.com>
2024-03-12 20:05:20 -0700

    last gdigit without e and q

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-    if letter == 'e' || letter == 'q':
-       break
-    print('{:c}'.format(letter), end='')
+for k in range(ord('a'), ord('z') + 1):
+    if let(k) != 'e' and let(k) != 'q':
+        print('{:c}'.format(i), end='')
