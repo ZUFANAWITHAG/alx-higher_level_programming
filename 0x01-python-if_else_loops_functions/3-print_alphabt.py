4f44d65ae42191d6ab8dc3de39d26e4df9cd77db
Zufan <zufanawushu@gmail.com>
2024-03-12 21:53:16 -0700

    3rd Task

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for k in range(ord('a'), ord('z') + 1):
     if let(k) != 'e' and let(k) != 'q':
-        print('{:c}'.format(i), end='')
+        print('{:c}'.format(k), end='')
