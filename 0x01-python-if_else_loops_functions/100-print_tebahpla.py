39fbf1d3e1e3f9904ee761e3741683ffcfa88d7e
Zufan <zufanawushu@gmail.com>
2024-03-13 15:01:57 -0700

    100 Task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for k in range(ord('z'), ord('a') - 1, -1):
-    if k % 2 == 0:
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
         diff = 0
     else:
         diff = 32
-    print('{}'.format(chr(k - diff)), end='')
+    print('{}'.format(chr(i - diff)), end='')
