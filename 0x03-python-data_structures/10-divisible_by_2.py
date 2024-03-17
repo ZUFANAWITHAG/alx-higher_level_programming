3e06f6a43c0dec96b7c73bd3173c941cc015686f
Zufan <zufanawushu@gmail.com>
2024-03-17 08:53:19 -0700

    divisible by two

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    new_list = []
-    if my_list:
-        my_list.sort(reverse=True)
-        return (my_list[0])
-    return (None)
+def divisible_by_2(my_list=[]):
+    mapped = map(lambda x: x % 2 == 0, my_list)
+    response = list(mapped)
+    return response
