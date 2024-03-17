a634866553afce50b5ada0842850ba84d6950938
Zufan <zufanawushu@gmail.com>
2024-03-17 08:50:40 -0700

    divide it by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
+def max_integer(my_list=[]):
     new_list = []
-    for i in new_list:
-        if (i % 2 == 0):
-            new_list.append(True)
-        else:
-            new_list.append(False)
-    return (new_list)
+    if my_list:
+        my_list.sort(reverse=True)
+        return (my_list[0])
+    return (None)
